f64a61caa6b8226e1e844cf2d8df5713ebd2347c
ngrosso1 <nicholasjgrosso@protonmail.com>
2025-11-04 00:13:49 -0500

    currently working

--- src/Contact.tsx
+++ src/Contact.tsx
@@ -4,11 +4,21 @@ import './Contact.scss'; // Ensure this file exists
 const Contact: React.FC = () => {
   return (
     <div className="contact">
-      <h2>Contact Info</h2>
-      <p>Email: nicholasj.grosso@gmail.com</p>
-      <p>Phone: (914)-564-6793</p>
-      <p>Address: Raleigh, NC</p>
-      {/* Add more contact information as needed */}
+      <h2>Contact Me</h2>
+      <p>
+        <span className="contact-label">Email</span>
+        <a href="mailto:nicholasj.grosso@gmail.com" className="contact-info">
+          nicholasj.grosso@gmail.com
+        </a>
+      </p>
+      <p>
+        <span className="contact-label">Phone</span>
+        <span className="contact-info">(914)-564-6793</span>
+      </p>
+      <p>
+        <span className="contact-label">Address</span>
+        <span className="contact-info">Raleigh, NC</span>
+      </p>
     </div>
   );
 };
