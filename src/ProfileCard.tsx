55251a9cd88d6be0cd2806b399c1af48b4bda0c5
ngrosso1 <nicholasjgrosso@protonmail.com>
2025-11-21 19:15:57 -0500

    updated resume

--- src/ProfileCard.tsx
+++ src/ProfileCard.tsx
@@ -15,7 +15,7 @@ const ProfileCard: React.FC = () => {
             Fullstack Software Engineer | IT Specialist
           </p>
           <div className="social-section">
-            <a href="https://drive.google.com/file/d/1LiGgF8i-nsC2FJBKAw82tcQGKYzDhW8p/view?usp=sharing" target="_blank" rel="noopener noreferrer" className="button-wrapper">
+            <a href="https://drive.google.com/file/d/1za1qE7yvZeFML-o72zC62SIEohcrTPWu/view?usp=sharing" target="_blank" rel="noopener noreferrer" className="button-wrapper">
               <button className="button-85" role="button">Resume</button>
             </a>
             <a href="mailto:nicholasj.grosso@gmail.com">
